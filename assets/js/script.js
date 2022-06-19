7ec052daf202704a5b22b49e6cc4b513ea3e8bd9
kaylab78 <kaylabackus78@gmail.com>
2022-06-18 21:07:56 -0500

    Pseudocode and variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var recipeTitleEl = document.getElementById("title");
+var ingredientEl = document.getElementById("ingredient");
+var addEl = document.getElementById("add-ingredient");
+var newRecipeEl = document.getElementById("new-recipe");
+
+// The user enters their ingredient into the form and clicks Add.
+// The ingredient is added to the recipe (a list).
+// The user creates a new recipe.
